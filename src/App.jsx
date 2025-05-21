d5da5aa34c1e2a7f23a03bc33b9903d17c5d44e0
copazeta <copazeta.ftv@gmail.com>
2025-05-21 11:19:39 -0300

    ajuste titulo

--- src/App.jsx
+++ src/App.jsx
@@ -172,4 +172,4 @@ export default App;
 
 
 // NÃO APAGAR - CÓDIGO PARA PUBLICAR AS ALTERAÇÕES
-// git add . && git commit -m "final_20250425" && git push
+// git add . && git commit -m "novos_players" && git push
